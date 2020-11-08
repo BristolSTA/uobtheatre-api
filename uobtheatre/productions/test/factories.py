6310f4c881cebf4a818e2c3e9181f391b482b9fd
jelgar <jamesnelgar@gmail.com>
2020-11-08 00:21:09 +0000

    Added tests for production serializer

---
+++ uobtheatre/productions/test/factories.py
@@ -0,0 +1,22 @@
+import factory
+
+from uobtheatre.productions.models import (
+    Society,
+    Production,
+) 
+
+
+class SocietyFactory(factory.django.DjangoModelFactory):
+    name = factory.Faker('sentence', nb_words=3)
+    class Meta:
+        model = Society 
+
+
+class ProductionFactory(factory.django.DjangoModelFactory):
+
+    name = factory.Faker('sentence', nb_words=3)
+    society = factory.SubFactory(SocietyFactory)
+    
+    class Meta:
+        model = Production
+
---
+++ uobtheatre/productions/test/test_serializers.py
@@ -0,0 +1,41 @@
+import pytest
+
+from uobtheatre.productions.test.factories import (
+    ProductionFactory,
+    SocietyFactory,
+)
+from uobtheatre.productions.serializers import (
+    ProductionSerializer,
+    SocietySerializer,
+)
+from uobtheatre.productions.models import (
+    Production,
+    Society,
+)
+
+@pytest.mark.django_db
+def test_society_serializer():
+    society = SocietyFactory()
+    data = Society.objects.first()
+    serialized_society = SocietySerializer(data) 
+
+    assert serialized_society.data == {
+        "id": society.id,
+        "name": society.name,
+    } 
+
+
+@pytest.mark.django_db
+def test_prodction_serializer():
+    production = ProductionFactory()
+    data = Production.objects.first()
+    serialized_prod = ProductionSerializer(data) 
+
+    assert serialized_prod.data == {
+        "id": production.id,
+        "name": production.name,
+        "society": {
+            "id": production.society.id,
+            "name": production.society.name,
+        },
+    } 
