468c1216c2a0e1a17bb501bef0df45bdbb6a2396
jelgar <jamesnelgar@gmail.com>
2020-11-08 13:20:33 +0000

    Updated view test

--- uobtheatre/productions/test/test_views.py
+++ uobtheatre/productions/test/test_views.py
@@ -9,7 +9,8 @@ from uobtheatre.productions.test.factories import (
 def test_production_view_get(api_client):
 
     # Create a fake production
-    prod = ProductionFactory()
+    prod1 = ProductionFactory()
+    prod2 = ProductionFactory()
 
     # Get the productions endpoint
     response = api_client.get(
@@ -19,16 +20,24 @@ def test_production_view_get(api_client):
     # Assert it returns 200 and what is expected
     assert response.status_code == 200 
     assert response.json() == {
-        'count': 1,
+        'count': 2,
         'next': None,
         'previous': None,
         'results': [
             {
-                'id': prod.id,
-                'name': prod.name,
+                'id': prod1.id,
+                'name': prod1.name,
                 'society': {
-                    'id': prod.society.id,
-                    'name': prod.society.name 
+                    'id': prod1.society.id,
+                    'name': prod1.society.name 
+                }
+            },
+            {
+                'id': prod2.id,
+                'name': prod2.name,
+                'society': {
+                    'id': prod2.society.id,
+                    'name': prod2.society.name 
                 }
             }
         ],
