541afcb9a2f8ee865a993faa7596b7dbdd974f93
jelgar <jamesnelgar@gmail.com>
2020-11-21 23:18:13 +0000

    Added some empty tests

--- uobtheatre/bookings/test/test_serializers.py
+++ uobtheatre/bookings/test/test_serializers.py
@@ -2,7 +2,11 @@ import pytest
 import uuid
 
 from uobtheatre.bookings.models import Booking
-from uobtheatre.bookings.serializers import BookingSerialiser
+from uobtheatre.bookings.serializers import (
+    BookingSerialiser,
+    CreateBookingSerialiser,
+    CreateSeatBookingSerializer,
+)
 from uobtheatre.bookings.test.factories import BookingFactory
 
 
@@ -30,3 +34,18 @@ def test_booking_serializer(date_format_2):
         "booking_reference": str(booking.booking_reference),
         "performance": performance,
     }
+
+
+@pytest.mark.skip(reason="Need to write this")
+@pytest.mark.django_db
+def test_create_booking_serializer(date_format_2):
+    booking = BookingFactory()
+    data = Booking.objects.first()
+    serialized_booking = CreateBookingSerialiser(data)
+
+
+@pytest.mark.skip(reason="Need to write this")
+def test_create_seat_booking_serializer(date_format_2):
+    seat_booking = SeatBooking()
+    data = SeatBooking.objects.first()
+    serialized_booking = CreateSeatBookingSerialiser(data)
