70999bf79cb4db3a28442383677172306df178c2
jelgar <jamesnelgar@gmail.com>
2020-11-21 15:13:04 +0000

    Cleaned up test views verbs test

--- uobtheatre/test/test_views.py
+++ uobtheatre/test/test_views.py
@@ -2,34 +2,36 @@ import pytest
 
 from uobtheatre.users.test.factories import UserFactory
 
-
-@pytest.mark.parametrize("endpoint", [("venues"), ("societies")])
-def test_view_is_read_only(api_client, endpoint):
-
-    response = api_client.post(f"/api/v1/{endpoint}/")
-    assert response.status_code == 405
-
-    response = api_client.put(f"/api/v1/{endpoint}/")
-    assert response.status_code == 405
-
-    response = api_client.delete(f"/api/v1/{endpoint}/")
-    assert response.status_code == 405
+ALL_HTTP_VERBS = ["GET", "POST", "PUT", "DELETE", "PATCH"]
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("endpoint", [("bookings")])
-def test_auth_view_is_read_only(api_client_authenticated, endpoint):
+@pytest.mark.parametrize(
+    "endpoint, allowed_verbs, auth_required",
+    [
+        ("venues", ["GET"], False),
+        ("societies", ["GET"], False),
+        ("bookings", ["GET"], True),
+        ("users", ["POST"], False),
+        ("productions", ["GET", "POST"], False),
+    ],
+)
+def test_views_only_allowed_required_verbs(
+    api_client, api_client_authenticated, endpoint, allowed_verbs, auth_required
+):
+    client = api_client if not auth_required else api_client_authenticated
+    disabled_verbs = [verb for verb in ALL_HTTP_VERBS if verb not in allowed_verbs]
 
-    response = api_client_authenticated.post(f"/api/v1/{endpoint}/")
-    assert response.status_code == 405
+    for verb in disabled_verbs:
+        response = getattr(client, verb.lower())(f"/api/v1/{endpoint}/")
+        assert response.status_code == 405
 
-    response = api_client_authenticated.put(f"/api/v1/{endpoint}/")
-    assert response.status_code == 405
-
-    response = api_client_authenticated.delete(f"/api/v1/{endpoint}/")
-    assert response.status_code == 405
+    for verb in allowed_verbs:
+        response = getattr(client, verb.lower())(f"/api/v1/{endpoint}/")
+        assert response.status_code != 405
 
 
+@pytest.mark.django_db
 @pytest.mark.parametrize("endpoint", [("bookings")])
 def test_requires_authentication(api_client, endpoint):
 
