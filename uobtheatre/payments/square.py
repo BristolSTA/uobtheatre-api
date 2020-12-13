e4dce378e8be4b23278ed54b8698dbd6f03dccee
jelgar <jamesnelgar@gmail.com>
2020-12-13 17:26:36 +0000

    Square customers

--- uobtheatre/payments/square.py
+++ uobtheatre/payments/square.py
@@ -11,6 +11,10 @@ class PaymentProvider:
             environment=SQUARE_SETTINGS["SQUARE_ENVIRONMENT"],
         )
 
+    """
+    Payments
+    """
+
     def list_payments(self):
         return self.client.payments.list_payments()
 
@@ -22,7 +26,27 @@ class PaymentProvider:
         }
         return self.client.payments.create_payment(body)
 
+    """
+    Customers
+    """
 
-def payment():
-    pp = PaymentProvider()
-    return pp.list_payments()
+    def create_customer(self):
+        body = {
+            "given_name": "James",
+        }
+        return self.client.customers.create_customer(body)
+
+    def create_customer_card(self, customer_id):
+        body = {
+            "card_nonse": "cnon:card-nonce-ok",
+            "billing_address": {
+                "address_line_1": "500 Electric Ave",
+                "address_line_2": "Suite 600",
+                "administrative_district_level_1": "NY",
+                "country": "US",
+                "locality": "New York",
+                "postal_code": "10003",
+            },
+            "cardholder_name": "Amelia Earhart",
+        }
+        return self.client.customers.create_customer(customer_id, body)
--- uobtheatre/payments/test/test_square.py
+++ uobtheatre/payments/test/test_square.py
@@ -5,10 +5,21 @@ def test_square_list_payments():
     pp = PaymentProvider()
     result = pp.list_payments()
     assert result.status_code == 200
-    assert result.body == {}
 
 
 def test_square_create_payment():
     pp = PaymentProvider()
     result = pp.create_payment()
+    assert result.status_code == 200
+
+
+def test_square_create_customer():
+    pp = PaymentProvider()
+    result = pp.create_customer()
+    assert result.status_code == 200
+
+
+def test_square_create_custom_card():
+    pp = PaymentProvider()
+    result = pp.create_customer()
     assert result.status_code == 200
