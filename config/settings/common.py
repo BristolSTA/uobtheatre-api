adfe1577815157a75f1fb4b28c3d921396c6f9a3
jelgar <jamesnelgar@gmail.com>
2020-12-16 21:36:32 +0000

    Removed default readonly permission class

--- config/settings/common.py
+++ config/settings/common.py
@@ -217,9 +217,7 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
     ),
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ],
+    "DEFAULT_PERMISSION_CLASSES": [],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
