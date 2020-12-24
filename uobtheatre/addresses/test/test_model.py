62b8af87100fb2d2c216f49ea40646b5487565ab
Alex Toff <me@alextoff.uk>
2020-12-24 13:32:45 +0000

    Rename the method

--- uobtheatre/addresses/test/test_model.py
+++ uobtheatre/addresses/test/test_model.py
@@ -4,7 +4,7 @@ from uobtheatre.addresses.test.factories import AddressFactory
 
 
 @pytest.mark.django_db
-def test_address_serializer():
+def test_address_to_string():
     address = AddressFactory()
 
     assert str(address) == f"{address.street}, {address.city}, {address.postcode}"
