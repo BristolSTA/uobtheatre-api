b60b2a2d700445d0d4981b23c72992894720f0d3
streth11 <tom.streuli@gmail.com>
2020-12-27 09:04:30 -0800

    added building number  and name

--- uobtheatre/addresses/models.py
+++ uobtheatre/addresses/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Address(models.Model):
     """ An address """
 
+    building_name = models.CharField(max_length=255)
+    building_number = models.CharField(max_length=10)
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     postcode = models.CharField(max_length=9)
--- uobtheatre/addresses/serializers.py
+++ uobtheatre/addresses/serializers.py
@@ -6,4 +6,4 @@ from uobtheatre.addresses.models import Address
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ["street", "city", "postcode", "latitude", "longitude"]
+        fields = ["building_name", "building_number", "street", "city", "postcode", "latitude", "longitude"]
