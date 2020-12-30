8104aa28744fceeeb40e4e3b7c58d826f331bc02
jelgar <jamesnelgar@gmail.com>
2020-12-30 12:04:35 +0000

    Added production seat group to seed

--- uobtheatre/productions/admin.py
+++ uobtheatre/productions/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from uobtheatre.productions.models import (CastMember, CrewMember, CrewRole,
-                                           Performance, Production, Society,
-                                           Warning)
+from uobtheatre.productions.models import (
+    CastMember,
+    CrewMember,
+    CrewRole,
+    Performance,
+    Production,
+    Society,
+    Warning,
+    PerformanceSeatGroup,
+)
 
 admin.site.register(Production)
 admin.site.register(Society)
@@ -11,3 +18,4 @@ admin.site.register(Warning)
 admin.site.register(CrewMember)
 admin.site.register(CastMember)
 admin.site.register(CrewRole)
+admin.site.register(PerformanceSeatGroup)
