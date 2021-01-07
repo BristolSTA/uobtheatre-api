9a506fe9a3fdcda90502b98950e35db1642f74bb
jelgar <jamesnelgar@gmail.com>
2021-01-07 13:38:27 +0000

    Added boto4 notes

--- uobtheatre/videos/bento.py
+++ uobtheatre/videos/bento.py
@@ -3,6 +3,29 @@ import subprocess
 from config.settings.common import BIN_ROOT, MEDIA_ROOT
 
 
+"""
+Bento is a powerful tool which we use to covert mp4s into encrypted mp4dash
+files. Currently we use the binaries from the bento sdk directly. This is a
+pretty horrible solution and hopefully we can swap these out for the c++/python
+libraries in the future. For now though we will have to make sure we install
+the binaries before working on the bento side of things.
+
+TODO:
+Find a way to store the bento stuff (probably the drive) with install
+instructions. For now the aim is to make bento stuff a seperate thing that most
+people wont care about. If someone want to work on the bento stuff it will
+require a couple of extra steps but then they should be good to go as well.
+
+NOTES:
+mp4fragment - can probably use Mp4Fragment.cpp directly, gonna need to do some
+reading
+https://realpython.com/python-bindings-overview/#python-bindings-overview 🥳
+
+mp4dash - https://github.com/axiomatic-systems/Bento4/issues/274
+mp4dash is written in python and depends on the c++ binaries (I think)
+"""
+
+
 def run(binary, args):
     """
     Run a binary, its pretty grim but for now does the trick
@@ -33,5 +56,7 @@ def mp4dash(fragmented_video_name: str):
             "--encryption-key",
             "A16E402B9056E371F36D348AA62BB749:87237D20A19F58A740C05684E699B4AA,A16E402B9056E371F36D348AA62BB749:87237D20A19F58A740C05684E699B4AA",
             f"{MEDIA_ROOT}/{fragmented_video_name}",
+            "-o",
+            f"{MEDIA_ROOT}/{fragmented_video_name}_output",
         ],
     )
