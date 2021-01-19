da695e12d06ba65a021b7fa430c98acd94c12e8e
jelgar <jamesnelgar@gmail.com>
2021-01-19 16:30:50 +0000

    Fixed tests

--- uobtheatre/productions/test/test_schema.py
+++ uobtheatre/productions/test/test_schema.py
@@ -86,16 +86,22 @@ def test_productions_schema(gql_client, gql_id):
 
 
 @pytest.mark.django_db
-def test_production_by_id(gql_client):
+def test_production_by_id(gql_client, gql_id):
     production = ProductionFactory()
 
-    query_string = "{ productions(id: %s) { id } }"
+    query_string = '{ production(id: "%s") { id } }'
 
-    response = gql_client.execute(query_string % (production.id + 1))
-    assert response["data"] == {"productions": []}
+    response = gql_client.execute(query_string % ("NotTheID"))
+    print(response)
+    print(query_string % ("NotTheID"))
+    assert response["data"] == {"production": None}
 
-    response = gql_client.execute(query_string % (production.id))
-    assert response["data"] == {"productions": [{"id": str(production.id)}]}
+    response = gql_client.execute(
+        query_string % (gql_id(production.id, "ProductionNode"))
+    )
+    assert response["data"] == {
+        "production": {"id": gql_id(production.id, "ProductionNode")}
+    }
 
 
 @pytest.mark.django_db
@@ -106,16 +112,20 @@ def test_performance_schema(gql_client, gql_id):
         """
         {
 	  performances {
-            capacity
-            doorsOpen
-            end
-            extraInformation
-            id
-            production {
-              id
+            edges {
+              node {
+                capacity
+                doorsOpen
+                end
+                extraInformation
+                id
+                production {
+                  id
+                }
+                start
+                capacityRemaining
+              }
             }
-            start
-            capacityRemaining
           }
         }
         """
@@ -123,19 +133,27 @@ def test_performance_schema(gql_client, gql_id):
 
     assert response == {
         "data": {
-            "performances": [
-                {
-                    "capacity": performance.capacity,
-                    "doorsOpen": performance.doors_open.isoformat(),
-                    "end": performance.end.isoformat(),
-                    "extraInformation": performance.extra_information,
-                    "id": gql_id(performances.id, "ProductionNode"),
-                    "production": {"id": str(performance.production.id)},
-                    "start": performance.start.isoformat(),
-                    "capacityRemaining": performance.capacity_remaining(),
-                }
-                for performance in performances
-            ]
+            "performances": {
+                "edges": [
+                    {
+                        "node": {
+                            "capacity": performance.capacity,
+                            "doorsOpen": performance.doors_open.isoformat(),
+                            "end": performance.end.isoformat(),
+                            "extraInformation": performance.extra_information,
+                            "id": gql_id(performance.id, "PerformanceNode"),
+                            "production": {
+                                "id": gql_id(
+                                    performance.production.id, "ProductionNode"
+                                )
+                            },
+                            "start": performance.start.isoformat(),
+                            "capacityRemaining": performance.capacity_remaining(),
+                        }
+                    }
+                    for performance in performances
+                ]
+            }
         }
     }
 
@@ -151,7 +169,11 @@ def test_performance_blocked_attributes(gql_client, attribute, is_obj):
     query_string = """
         {
             performances {
-                %s
+              edges {
+                node {
+                  %s
+                }
+              }
             }
         }
         """ % (
@@ -161,5 +183,5 @@ def test_performance_blocked_attributes(gql_client, attribute, is_obj):
     response = gql_client.execute(query_string)
     assert (
         response["errors"][0]["message"]
-        == f'Cannot query field "{attribute}" on type "PerformanceType".'
+        == f'Cannot query field "{attribute}" on type "PerformanceNode".'
     )
