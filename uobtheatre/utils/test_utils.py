f3eb7bae4459b3cb273c915dff670695d7480812
Kit Walker <se19759@bristol.ac.uk>
2021-03-18 22:39:01 +0000

    implementing a tasty suggestion from James

--- uobtheatre/utils/test_utils.py
+++ uobtheatre/utils/test_utils.py
@@ -1,16 +1,9 @@
+from uobtheatre.bookings.models import Ticket
+
+
 def ticketDictListDictGen(ticketDictList):
-    ticketDict = {}
-    for ticket in ticketDictList:
-        ticketKey = (
-            ticket.get("seat_group_id"),
-            ticket.get("concession_type_id"),
-            ticket.get("seat_id"),
-        )
-        if ticketKey in ticketDict:
-            ticketDict[ticketKey] += 1
-        else:
-            ticketDict[ticketKey] = 1
-    return ticketDict
+    ticketObjectList = [Ticket(**ticketDict) for ticketDict in ticketDictList]
+    return ticketListDictGen(ticketObjectList)
 
 
 def ticketListDictGen(ticketList):
