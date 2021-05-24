2f2174e840aecbbfc9f26f6928cf1eb6d829900d
jelgar <jamesnelgar@gmail.com>
2021-05-24 21:44:11 +0100

    Set experiation of jwt to 5 mins

--- config/settings/common.py
+++ config/settings/common.py
@@ -262,7 +262,7 @@ GRAPHQL_JWT = {
     ],
     "JWT_VERIFY_EXPIRATION": True,
     "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
-    "JWT_EXPIRATION_DELTA": timedelta(minutes=1),
+    "JWT_EXPIRATION_DELTA": timedelta(minutes=5),
     "JWT_REFRESH_EXPIRATION_DELTA": timedelta(days=365),
 }
 
