554bc6e33f6a71b19f9a3f679b9e1f108ceea23c
James Elgar <jamesnelgar@gmail.com>
2021-06-28 19:40:04 +0100

    More tests

--- uobtheatre/users/test/test_models.py
+++ uobtheatre/users/test/test_models.py
@@ -1,9 +1,10 @@
 import pytest
 
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, Group
 
 from uobtheatre.users.test.factories import UserFactory
 from uobtheatre.productions.test.factories import ProductionFactory
+from uobtheatre.users.models import User
 from conftest import AuthenticateableGQLClient
 from guardian.shortcuts import assign_perm
 
@@ -27,10 +28,12 @@ def test_boxoffice_permissions_object_level(gql_client_flexible: Authenticateabl
 def test_boxoffice_permissions_model_level(gql_client_flexible: AuthenticateableGQLClient):
     production = ProductionFactory()
     user = gql_client_flexible.user
+    assert not user.has_perm('productions.boxoffice')
     assert not user.has_perm('productions.boxoffice', production)
 
     boxoffice_perm = Permission.objects.get(codename='boxoffice')
     user.user_permissions.add(boxoffice_perm)
+    user = User.objects.get(pk=user.pk)
 
     assert user.has_perm('productions.boxoffice')
     assert user.has_perm('productions.boxoffice', production)
@@ -39,11 +42,15 @@ def test_boxoffice_permissions_model_level(gql_client_flexible: Authenticateable
 @pytest.mark.django_db
 def test_boxoffice_permissions_model_level_group(gql_client_flexible: AuthenticateableGQLClient):
     production = ProductionFactory()
+    group = Group.objects.create(name='TestGroup')
+    boxoffice_perm = Permission.objects.get(codename='boxoffice')
+    group.permissions.add(boxoffice_perm)
+
     user = gql_client_flexible.user
     assert not user.has_perm('productions.boxoffice', production)
 
-    boxoffice_perm = Permission.objects.get(codename='boxoffice')
-    user.user_permissions.add(boxoffice_perm)
+    user.groups.add(group)
+    user = User.objects.get(pk=user.pk)
 
     assert user.has_perm('productions.boxoffice')
     assert user.has_perm('productions.boxoffice', production)
