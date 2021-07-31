b712c6d9f67e8cdb037d4decd50f3f1d82d0d554
Kit Walker <kitwalker@Kits-MacBook-Pro.local>
2021-07-31 11:39:38 +0100

    gen device code

--- uobtheatre/payments/schema.py
+++ uobtheatre/payments/schema.py
@@ -31,3 +31,26 @@ class PaymentNode(GrapheneEnumMixin, DjangoObjectType):
         interfaces = (relay.Node,)
         filterset_class = PaymentFilter
         exclude = ("pay_object_id", "pay_object_type")
+class CreateDeviceCode(AuthRequiredMixin, SafeMutation):
+    """Mutation to generate device code using name.
+
+    Args:
+        name (str): The name of the device
+
+    Returns:
+        code (str): Returns the code of the device
+
+    Raises:
+    """
+
+    code = graphene.String()
+
+    class Arguments:
+        name = graphene.String(required=True)
+
+    @classmethod
+    def resolve_mutation(cls, _, name):
+        payment_provider = PaymentProvider()
+        code = paymnet_provider.create_device_code(name)
+    
+    return CreateDeviceCode(code=code)
