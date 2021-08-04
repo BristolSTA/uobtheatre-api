c651d67adcfa567b1c58e56bc7f96acb5273b7be
James Elgar <jamesnelgar@gmail.com>
2021-08-04 21:34:40 +0100

    Fixes/prod env (#220)
    * Adding anymail to common (temporary)
    
    * Activation email
    
    * Update configs
    
    * Updated email display name
    
    * Added test for email verification being required for login
    
    * Generate schema
    
    * Fixes for prod
    
    * CORS
    
    * Added staging url to allowed cors
    
    * Getting pretty baffled

--- config/settings/common.py
+++ config/settings/common.py
@@ -50,12 +50,11 @@ INSTALLED_APPS = (
 MIDDLEWARE = (
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
 )
 
