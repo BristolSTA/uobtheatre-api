beb63e2bfebf8e5e449b6d78de1dcc49a4cc5cf8
Alex Toff <me@alextoff.uk>
2021-11-09 17:18:25 +0000

    Fix unique validation

--- uobtheatre/discounts/models.py
+++ uobtheatre/discounts/models.py
@@ -90,6 +90,7 @@ class Discount(models.Model):
             discount
             for discount in discounts
             if discount.get_concession_map() == self.get_concession_map()
+            and discount.percentage == self.percentage
         ]
         discounts_with_same_requirements_names = [
             discount.name for discount in discounts_with_same_requirements
