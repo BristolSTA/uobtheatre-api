4fa55db5665c5b18f60b541d9d7b7e89820921ed
Alex Toff <me@alextoff.uk>
2021-11-10 21:40:00 +0000

    address comment

--- uobtheatre/discounts/test/test_models.py
+++ uobtheatre/discounts/test/test_models.py
@@ -7,12 +7,16 @@ from uobtheatre.discounts.test.factories import (
     DiscountFactory,
     DiscountRequirementFactory,
 )
+from uobtheatre.productions.test.factories import PerformanceFactory
 
 
 @pytest.mark.django_db
 def test_cannot_create_2_discounts_with_the_same_requirements():
     dis_1 = DiscountFactory()
     dis_2 = DiscountFactory()
+    perf = PerformanceFactory()
+    dis_1.performances.set([perf])
+    dis_2.performances.set([perf])
 
     requirement_1 = DiscountRequirementFactory(discount=dis_1)
 
@@ -28,19 +32,28 @@ def test_cannot_create_2_discounts_with_the_same_requirements():
     with pytest.raises(ValidationError):
         dis_1.validate_unique()
 
-    # Change the percentage, to make unique
-    dis_1.percentage = 0.3
+    # Change the performance, to make unique
+    dis_1.performances.set([PerformanceFactory()])
 
     dis_1.validate_unique()
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("num_requirements", [0, 1, 2])
-def test_discount_with_same_requirements_is_not_unique(num_requirements):
+@pytest.mark.parametrize(
+    "num_requirements,same_performance",
+    [(0, False), (1, False), (2, False), (0, True), (1, True), (2, True)],
+)
+def test_discount_with_same_requirements_same_performance(
+    num_requirements, same_performance
+):
     concessions = [ConcessionTypeFactory() for _ in range(num_requirements)]
+    performance_1 = PerformanceFactory()
+    performance_2 = PerformanceFactory()
 
-    dis_1 = DiscountFactory(percentage=0.2)
-    dis_2 = DiscountFactory(percentage=0.2)
+    dis_1 = DiscountFactory()
+    dis_2 = DiscountFactory()
+    dis_1.performances.set([performance_1])
+    dis_2.performances.set([performance_1 if same_performance else performance_2])
 
     dis_1.requirements.set(
         [
@@ -56,14 +69,16 @@ def test_discount_with_same_requirements_is_not_unique(num_requirements):
         bulk=False,
     )
 
-    with pytest.raises(ValidationError):
+    if same_performance:
+        with pytest.raises(ValidationError):
+            dis_2.validate_unique()
+    else:
         dis_2.validate_unique()
 
 
 @pytest.mark.django_db
 def test_discount_without_exclusion_unique_requirement():
-    DiscountFactory(percentage=0.2)
-    dis_2 = Discount(percentage=0.2)
+    DiscountFactory()
+    dis_2 = Discount()
 
-    with pytest.raises(ValidationError):
-        dis_2.validate_unique()
+    dis_2.validate_unique()
