3ef4601ef3ff32b39b8ae6ec6f565bcf5fbcb86d
jelgar <jamesnelgar@gmail.com>
2021-11-22 19:28:35 +0000

    Fixed payment sync payment action

--- uobtheatre/payments/admin.py
+++ uobtheatre/payments/admin.py
@@ -6,12 +6,17 @@ from uobtheatre.payments.models import Payment
 @admin.action(description="Refresh payment from square")  # type: ignore
 def refresh_from_square(modeladmin, request, queryset):
     for payment in queryset.all():
-        payment.update_from_square()
-        modeladmin.message_user(request, f"{queryset.count()} payments updated.")
+        payment.sync_payment_with_provider()
+    modeladmin.message_user(request, f"{queryset.count()} payments refreshed.")
 
 
 class PaymentAdmin(admin.ModelAdmin):
     actions = [refresh_from_square]
+    list_filter = (
+        "type",
+        "status",
+        ("provider_fee", admin.EmptyFieldListFilter),  # type: ignore
+    )
 
 
 admin.site.register(Payment, PaymentAdmin)
