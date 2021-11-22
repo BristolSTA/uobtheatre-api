fc5ecf14aaf8bf95ffc7669fcf1ca24873c29316
jelgar <jamesnelgar@gmail.com>
2021-11-22 19:35:50 +0000

    Better admin logging

--- uobtheatre/payments/admin.py
+++ uobtheatre/payments/admin.py
@@ -1,13 +1,26 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from uobtheatre.payments.models import Payment
+from uobtheatre.utils.exceptions import SquareException
 
 
 @admin.action(description="Refresh payment from square")  # type: ignore
 def refresh_from_square(modeladmin, request, queryset):
+    """
+    Refresh payment details from provider
+    """
+    successful_updates = 0
     for payment in queryset.all():
-        payment.sync_payment_with_provider()
-    modeladmin.message_user(request, f"{queryset.count()} payments refreshed.")
+        try:
+            payment.sync_payment_with_provider()
+            successful_updates += 1
+        except SquareException as exc:
+            modeladmin.message_user(
+                request,
+                f"Error updating payment {payment.id}: {exc.message}",
+                level=messages.ERROR,
+            )
+    modeladmin.message_user(request, f"{successful_updates} payments refreshed.")
 
 
 class PaymentAdmin(admin.ModelAdmin):
