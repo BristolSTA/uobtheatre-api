4c4f9bf156193d834d4b9c1006dc836b6f77b1de
Alex Toff <me@alextoff.uk>
2022-01-17 19:41:29 +0000

    create ability

--- uobtheatre/productions/abilities.py
+++ uobtheatre/productions/abilities.py
@@ -1,3 +1,4 @@
+from uobtheatre.productions.models import Performance
 from uobtheatre.users.abilities import Ability
 
 
@@ -44,5 +45,34 @@ class EditProduction(Ability):
             or (
                 user.has_perm("productions.approve_production", obj)
                 and obj.status == Production.Status.PENDING
+            )
+        )
+
+
+class BookForPerformance(Ability):
+    """Determine if the user can book for performances"""
+
+    name = "book"
+
+    @staticmethod
+    def user_has(_):
+        return True
+
+    @staticmethod
+    def user_has_for(user, obj: Performance) -> bool:
+        from uobtheatre.productions.models import Production
+
+        return obj.is_bookable and (
+            obj.production.status
+            == Production.Status.PUBLISHED  # Must be bookable and published
+            or (
+                EditProduction.user_has_for(
+                    user, obj
+                )  # If the user can edit the production
+                and obj.production.status
+                in [
+                    Production.Status.APPROVED,
+                    Production.Status.PUBLISHED,
+                ]  # and the status is approved or published (e.g. creating comps)
             )
         )
