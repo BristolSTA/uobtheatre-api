8e427101fcf080518f1967725062d7894dd27ffe
Alex Toff <me@alextoff.uk>
2022-01-24 18:24:04 +0000

    finish coverage

---
+++ uobtheatre/productions/test/test_tasks.py
@@ -0,0 +1,25 @@
+from unittest.mock import patch
+
+import pytest
+
+from uobtheatre.bookings.test.factories import BookingFactory
+from uobtheatre.productions.tasks import refund_performance
+from uobtheatre.productions.test.factories import PerformanceFactory
+from uobtheatre.users.test.factories import UserFactory
+
+
+@pytest.mark.django_db
+def test_refund_performance_task(mailoutbox):
+    performance = PerformanceFactory()
+    booking = BookingFactory(performance=performance)
+    BookingFactory()
+    auth_user = UserFactory()
+
+    with patch(
+        "uobtheatre.bookings.models.Booking.async_refund", autospec=True
+    ) as refund_mock:
+        refund_performance(performance.pk, auth_user.pk)
+
+    refund_mock.assert_called_once_with(booking, authorizing_user=auth_user)
+    assert len(mailoutbox) == 1
+    assert mailoutbox[0].subject == "[UOBTheatre] Performance Refunds Initiated"
