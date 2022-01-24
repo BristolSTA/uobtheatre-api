8c17f93c7812102b2e0d7ab04c7634bdb380584c
James Elgar <jamesnelgar@gmail.com>
2022-01-24 22:34:31 +0000

    Update uobtheatre/utils/tasks.py

--- uobtheatre/utils/tasks.py
+++ uobtheatre/utils/tasks.py
@@ -4,9 +4,6 @@ from celery import Task
 from django.core.mail import mail_admins
 from sentry_sdk import capture_exception
 
-if TYPE_CHECKING:
-    pass
-
 # Tasks
 class BaseTask(Task):
     def on_failure(self, exc, task_id, args, kwargs, einfo):
