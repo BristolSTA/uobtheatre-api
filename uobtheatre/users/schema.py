b2698c0d32672a7f9cba6db2fd521970ee0fb308
Alex Toff <me@alextoff.uk>
2022-02-11 15:07:32 +0000

    Only expose certain fields

--- uobtheatre/users/schema.py
+++ uobtheatre/users/schema.py
@@ -27,6 +27,16 @@ class ExtendedUserNode(schema.UserNode):
 
     class Meta:
         model = User
+        fields = (
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "date_joined",
+            "bookings",
+            "createdBookings",
+            "permissions",
+        )
 
 
 class AuthMutation(graphene.ObjectType):
