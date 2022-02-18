3c775ad9239d47e9309684f232f1f4832ac704c5
Alex Toff <me@alextoff.uk>
2022-02-18 02:27:43 +0000

    fix

--- uobtheatre/utils/test/test_forms.py
+++ uobtheatre/utils/test/test_forms.py
@@ -58,9 +58,9 @@ def test_send_email_form_submit(is_valid, user_reasons_generator, user_reason):
         {
             "message": "My Message",
             "subject": "My Subject",
+            "users": [UserFactory()],
         },
         initial={
-            "users": [UserFactory()],
             "user_reason": user_reason,
             "lgtm": True,
         },
