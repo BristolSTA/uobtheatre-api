5ef2ba0977a3f160828cd4ddaea4cbe39d6d00ed
Alex Toff <me@alextoff.uk>
2022-02-18 00:36:45 +0000

    Add test for email form submit

--- uobtheatre/utils/test/test_forms.py
+++ uobtheatre/utils/test/test_forms.py
@@ -1,6 +1,32 @@
+from unittest.mock import patch
+
 import pytest
+from django.core.exceptions import ValidationError
+
+from uobtheatre.users.test.factories import UserFactory
+from uobtheatre.utils.forms import SendEmailForm
 
 
 @pytest.mark.django_db
-def test_send_email_form_submit():
-    pass
+@pytest.mark.parametrize("is_valid", [True, False])
+def test_send_email_form_submit(is_valid):
+    form = SendEmailForm(
+        {
+            "message": "My Message",
+            "subject": "My Subject",
+        },
+        initial={
+            "users": [UserFactory()],
+            "user_reason": "My reason",
+            "lgtm": True,
+        },
+    )
+    form.is_valid()
+    with patch.object(form, "is_valid", return_value=is_valid):
+        if not is_valid:
+            with pytest.raises(ValidationError):
+                form.submit()
+        else:
+            with patch("uobtheatre.mail.composer.MassMailComposer.send") as send_mock:
+                form.submit()
+            send_mock.assert_called_once()
