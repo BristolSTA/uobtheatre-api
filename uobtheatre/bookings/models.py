dfd5cfe11c2a9b3400d2f2aee9579fee7964ea83
Guidedriver0 <a.yigit.ince@gmail.com>
2024-02-17 00:39:54 +0000

    woops

--- uobtheatre/bookings/models.py
+++ uobtheatre/bookings/models.py
@@ -548,9 +548,9 @@ class Booking(TimeStampedMixin, Payable):
         """
         super().complete()
 
-        # booking_emails.send_booking_confirmation_email(self, payment)
-        # if self.accessibility_info:
-        #     booking_emails.send_booking_accessibility_info_email(self)
+        booking_emails.send_booking_confirmation_email(self, payment)
+        if self.accessibility_info:
+            booking_emails.send_booking_accessibility_info_email(self)
 
     def clone(self):
         clone = super().clone()
