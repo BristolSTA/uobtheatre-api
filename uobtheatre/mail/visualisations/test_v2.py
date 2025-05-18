8c6767c38960bd48526b1308804e5b75c8738586
Zack Colton <zack@familycolton.co.uk>
2025-05-18 19:16:45 +0000

    Lots of small tweaks; also make using colstacks easier

--- uobtheatre/mail/visualisations/test_v2.py
+++ uobtheatre/mail/visualisations/test_v2.py
@@ -16,6 +16,19 @@ root = "./uobtheatre/mail/visualisations/v2/"
 
 mountainImage = "https://media.gettyimages.com/id/1211045588/photo/beautiful-winter-mountain-landscape-with-snow-and-glacier-lake.jpg?b=1&s=1024x1024&w=gi&k=20&c=gjiSUA2OVP3s8zJkmJDsBL2V80Hprvjdv5ZdEkV3VuE="
 
+# Icons have to be SVG paths
+# https://fontawesome.com/search?q=code&o=r&ic=free
+icons = {
+    "clock": "M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z",
+    "search": "M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z",
+    "door-open": "M320 32c0-9.9-4.5-19.2-12.3-25.2S289.8-1.4 280.2 1l-179.9 45C79 51.3 64 70.5 64 92.5L64 448l-32 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0 192 0 32 0 0-32 0-448zM256 256c0 17.7-10.7 32-24 32s-24-14.3-24-32s10.7-32 24-32s24 14.3 24 32zm96-128l96 0 0 352c0 17.7 14.3 32 32 32l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-32 0 0-320c0-35.3-28.7-64-64-64l-96 0 0 64z",
+    "play": "M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z",
+    "barcode": "M0 80C0 53.5 21.5 32 48 32l96 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-96 0c-26.5 0-48-21.5-48-48L0 80zM64 96l0 64 64 0 0-64L64 96zM0 336c0-26.5 21.5-48 48-48l96 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-96 0c-26.5 0-48-21.5-48-48l0-96zm64 16l0 64 64 0 0-64-64 0zM304 32l96 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-96 0c-26.5 0-48-21.5-48-48l0-96c0-26.5 21.5-48 48-48zm80 64l-64 0 0 64 64 0 0-64zM256 304c0-8.8 7.2-16 16-16l64 0c8.8 0 16 7.2 16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s7.2-16 16-16s16 7.2 16 16l0 96c0 8.8-7.2 16-16 16l-64 0c-8.8 0-16-7.2-16-16s-7.2-16-16-16s-16 7.2-16 16l0 64c0 8.8-7.2 16-16 16l-32 0c-8.8 0-16-7.2-16-16l0-160zM368 480a16 16 0 1 1 0-32 16 16 0 1 1 0 32zm64 0a16 16 0 1 1 0-32 16 16 0 1 1 0 32z",
+    "accessibility": "M192 96a48 48 0 1 0 0-96 48 48 0 1 0 0 96zM120.5 247.2c12.4-4.7 18.7-18.5 14-30.9s-18.5-18.7-30.9-14C43.1 225.1 0 283.5 0 352c0 88.4 71.6 160 160 160c61.2 0 114.3-34.3 141.2-84.7c6.2-11.7 1.8-26.2-9.9-32.5s-26.2-1.8-32.5 9.9C240 440 202.8 464 160 464C98.1 464 48 413.9 48 352c0-47.9 30.1-88.8 72.5-104.8zM259.8 176l-1.9-9.7c-4.5-22.3-24-38.3-46.8-38.3c-30.1 0-52.7 27.5-46.8 57l23.1 115.5c6 29.9 32.2 51.4 62.8 51.4l5.1 0c.4 0 .8 0 1.3 0l94.1 0c6.7 0 12.6 4.1 15 10.4L402 459.2c6 16.1 23.8 24.6 40.1 19.1l48-16c16.8-5.6 25.8-23.7 20.2-40.5s-23.7-25.8-40.5-20.2l-18.7 6.2-25.5-68c-11.7-31.2-41.6-51.9-74.9-51.9l-68.5 0-9.6-48 63.4 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-76.2 0z",
+    "money": "M96 96l0 224c0 35.3 28.7 64 64 64l416 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L160 32c-35.3 0-64 28.7-64 64zm64 160c35.3 0 64 28.7 64 64l-64 0 0-64zM224 96c0 35.3-28.7 64-64 64l0-64 64 0zM576 256l0 64-64 0c0-35.3 28.7-64 64-64zM512 96l64 0 0 64c-35.3 0-64-28.7-64-64zM288 208a80 80 0 1 1 160 0 80 80 0 1 1 -160 0zM48 120c0-13.3-10.7-24-24-24S0 106.7 0 120L0 360c0 66.3 53.7 120 120 120l400 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-400 0c-39.8 0-72-32.2-72-72l0-240z",
+    "card": "M64 32C28.7 32 0 60.7 0 96l0 32 576 0 0-32c0-35.3-28.7-64-64-64L64 32zM576 224L0 224 0 416c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-192zM112 352l64 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-64 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm112 16c0-8.8 7.2-16 16-16l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16z",
+    "trolley": "M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"
+}
 
 def write_html_file(mail, filename):
     # Delete the existing file
@@ -28,7 +41,7 @@ def write_html_file(mail, filename):
 
 
 @pytest.mark.django_db
-def test_simple_email():
+def _test_simple_email():
 
     test_mail = (
         MailComposer.blank([Box(
@@ -46,7 +59,7 @@ def test_simple_email():
 
 
 @pytest.mark.django_db
-def test_text_only():
+def _test_text_only():
 
     test_mail = MailComposer.textOnly(
         "This is a test title", "<b>This</b> is a test message that's actually longer than you would expect it to be because it's important for the sake of testing that we have a really long message here that spans multiple lines.", True)
@@ -55,7 +68,7 @@ def test_text_only():
 
 
 @pytest.mark.django_db
-def test_booking_conf():
+def _test_booking_conf():
 
     booking = BookingFactory()
     payment = TransactionFactory()
@@ -98,3 +111,64 @@ def test_booking_conf():
     )
 
     write_html_file(test_mail, "booking_conf.html")
+
+
+@pytest.mark.django_db
+def test_booking_conf_new():
+
+    booking = BookingFactory()
+    payment = TransactionFactory()
+
+    doors = booking.performance.doors_open.astimezone(
+        booking.performance.venue.address.timezone).strftime('%d %B %Y %H:%M %Z')
+    start = booking.performance.start.astimezone(
+        booking.performance.venue.address.timezone).strftime('%H:%M %Z')
+
+    test_mail = MailComposer.blank([
+
+        Paragraph(
+            title="Your booking to %s has been confirmed!" % booking.performance.production.name
+        ),
+
+        Image(src=mountainImage),
+
+        BoxCols(
+            [
+
+                RowStack([Paragraph(title="Timings",
+                                    message=f"Doors Open: {doors}", titleIcon=icons["clock"], messageIcon=icons["door-open"]),
+
+                          Paragraph(
+                    message=f"Performance Starts: {start}", messageIcon=icons["play"])
+
+                ]),
+
+                RowStack([Paragraph(title="Your Booking",
+                                    message=booking.reference, titleIcon=icons["search"], messageIcon=icons["barcode"]),
+
+                          Button(booking.web_tickets_path, "View Tickets"),
+
+                          Button("/user/booking/%s" %
+                                 booking.reference, "View Booking")
+
+                          ])
+            ]
+        ),
+
+        BoxCols([
+
+            RowStack([
+                Paragraph(title="Payment Information", message=f"{payment.value_currency} paid", titleIcon=icons["trolley"], messageIcon=icons["money"]
+                          ),
+
+                Paragraph(
+                    message=f"{payment.provider.description}{' - ID ' + payment.provider_transaction_id if payment.provider_transaction_id else '' }", messageIcon=icons["card"]),
+            ]),
+
+            Paragraph(title="Accessibility Information", titleIcon=icons["accessibility"],
+                      message="If you have any accessibility concerns, or otherwise need help, please contact <a href='mailto:support@uobtheatre.com'>support@uobtheatre.com</a>.", html=True)
+        ])
+    ]
+    )
+
+    write_html_file(test_mail, "booking_conf_new.html")
